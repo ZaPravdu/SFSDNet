39215bfefb958f9b2b9cca76227fed16dea4db69
ZaPravdu <373050478@qq.com>
2026-06-27 20:34:37 +0800

    script

--- train_script.sh
+++ train_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /home/mscs/houminqiu2/SFSDNet
-# /home/mscs/houminqiu2/miniconda3/envs/VIC/bin/python3 train_variance_reg.py
-/home/mscs/houminqiu2/miniconda3/envs/VIC/bin/python3 train_p2r.py
+# 使用 $@ 转发 condor arguments 中的命令行参数
+/home/mscs/houminqiu2/miniconda3/envs/VIC/bin/python3 train_p2r.py "$@"
